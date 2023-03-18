80db06a54a755f3f97e9acb4961724c6364b0d3f
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2023-03-19 00:59:47 +0100

    Background gradients goes to black

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -6,7 +6,7 @@ type AppProps = {
 
 const App = ({ children }:AppProps) => {
 	return (
-		<div class="bg-gradient-to-b from-violet-800 to-purple-900 flex flex-col w-full overflow-x-hidden" style="min-height: 100vh;">
+		<div class="bg-gradient-to-b from-violet-900 to-black flex flex-col w-full overflow-x-hidden" style="min-height: 100vh;">
 			{children}
 		</div>
 	)
