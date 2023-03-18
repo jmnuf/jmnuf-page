59a95a5a5e51e495fb91adbdfdbc58765744f90f
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2023-03-18 20:26:29 +0100

    SEO? Idk man

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
 		<>
 			<div class="w-full text-pink-200 pb-4 shadow-inner mb-2 pt-1 bg-gray-800 bg-opacity-50 text-center">
 				<Carousel slideDurationSeconds={10} startingSlide={1}>
-					<img src="/logo-1.png" alt="jm logo 1" class="block h-44" />
-					<img src="/logo-2.png" alt="jm logo 2" class="block h-44" />
+					<img src="/logo-1.png" alt="jm logo 1" width={176} height={176} class="block h-44" />
+					<img src="/logo-2.png" alt="jm logo 2" width={176} height={176} class="block h-44" />
 				</Carousel>
 				<h1>JM</h1>
 				<h2>A guy that exists and does stuff sometimes</h2>
