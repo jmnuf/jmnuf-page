758a5281f024e63a8ad7536942ec3ebb174f6b49
Jose Marco Nufio Garrido <jose.m.nufio.0@gmail.com>
2024-11-06 21:27:33 +0100

    Add home icon onto link for home in 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,14 +1,18 @@
 import Link from "next/link";
 
+import { HomeSVG } from "~/app/_components/icons";
+
 export default function NotFound() {
   return (
     <div className="flex h-full w-full flex-col items-center justify-center">
       <h1 className="text-2xl font-bold">404 - Page Not Found</h1>
       <Link
-        className="text-lg font-medium text-sky-800 underline decoration-sky-800 hover:text-sky-400 hover:decoration-sky-300"
+        className="flex flex-col items-center text-center text-lg font-medium text-sky-800 underline decoration-sky-800 hover:text-sky-400 hover:decoration-sky-300"
         href="/"
       >
         Go back Home
+        <br />
+        <HomeSVG size="50px" />
       </Link>
     </div>
   );
